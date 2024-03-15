84cefdaa415ab5bfe410b5b631e4426f21e34631
Lokesh <wanilokesh095@gmail.com>
2024-03-15 17:07:03 +0530

    Latest changes

---
+++ src/components/Builder.js
@@ -0,0 +1,37 @@
+import { Form, FormBuilder } from "@formio/react";
+import { useState } from "react";
+import { Card } from "react-bootstrap";
+import ReactJson from "react-json-view";
+import IndianStatesSelect from "./customComponent/IndianStatesSelect "; // Import your custom component
+import "../styles/Builder.css";
+
+const Builder = () => {
+  const [jsonSchema, setSchema] = useState({ components: [] });
+  const onFormChange = (schema) => {
+    setSchema({ ...schema, components: [...schema.components] });
+  };
+  return (
+    <>
+      {/* Register the custom component within the FormBuilder */}
+      <FormBuilder
+        form={jsonSchema}
+        onChange={onFormChange}
+        customComponents={{ IndianStatesSelect }}
+      />
+      <Card title="Form JSON Schema" className="my-4">
+        <Card.Body>
+          <Card.Title className="text-center">As JSON Schema</Card.Title>
+          <ReactJson src={jsonSchema} name={null} collapsed={true}></ReactJson>
+        </Card.Body>
+      </Card>
+      <Card className="my-4">
+        <Card.Body>
+          <Card.Title className="text-center">As Rendered Form</Card.Title>
+          <Form form={jsonSchema} />
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default Builder;
---
+++ src/components/customComponent/IndianStatesSelect .js
@@ -0,0 +1,72 @@
+import { Formio, Components } from "formiojs";
+import indianState from "../api-options/indianState.json";
+const FieldComponent = Components.components.field;
+
+export default class IndianStatesSelect extends FieldComponent {
+  constructor(component, options, data) {
+    super(component, options, data);
+    this.state = { options: [] }; // Add state to store fetched options
+  }
+
+  static schema() {
+    return FieldComponent.schema({
+      type: "select",
+      label: "Select Indian State",
+      key: "indianStates",
+      dataSrc: "url", // Set data source to URL
+      data: {
+        // url: 'https://mocki.io/v1/3bc06b43-9c01-4b27-802d-1edf295ed97e', // No URL needed as fetching from local file
+      },
+    });
+  }
+
+  static builderInfo = {
+    title: "Indian States",
+    group: "basic",
+    icon: "fa fa-map",
+    weight: 10,
+    documentation: "http://link.to.documentation",
+    schema: IndianStatesSelect.schema(),
+  };
+
+  //  attached() {
+  //   super.attached();
+  //   // console.log('Fetching options from:', 'http://localhost:8080/indianStates.json');
+  //   // const response = await fetch('http://localhost:8080/indianStates.json');
+  //   // const states = await response.json();
+  //   // this.setState({ options: states }); // Update state with fetched options
+  //   this.setState({ options: {
+  //     "name" : "LearnBestCoding",
+  //     "website" : "www.learnbestcoding.com",
+  //     "email" : "something@learnbestcoding.com",
+  //     "country" :[
+  //         {"value": "USA", "name" : "USA"},
+  //         {"value": "USA", "name" : "Canada"},
+  //         {"value": "RPC", "name" : "Japan"},
+  //         {"value": "ARATA", "name" : "Australia"}
+  //     ]
+  //   } });
+  //   // this.setState({ options: indianStates });
+  //   // console.log('Rendering IndianStatesSelect',indianState);
+
+  // }
+
+  render() {
+    // Override render to use options from state
+    return this.renderTemplate("select", {
+      input: `<select class="form-control" ref="select">
+        ${this.state.options
+          .map(
+            (state) => `<option value="${state.value}">${state.name}</option>`
+          )
+          .join("")}
+      </select>`,
+    });
+  }
+}
+
+Formio.use({
+  components: {
+    IndianStatesSelect: IndianStatesSelect,
+  },
+});
